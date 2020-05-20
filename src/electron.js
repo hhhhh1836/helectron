91a455e72aa1cf4e0d00e35a71dd9edec98f8389
Senko <not_a_senko_tank@outlook.com>
2020-05-20 18:07:13 +0300

    h

--- src/electron.js
+++ src/electron.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron');
+const { app, window, BrowserWindow } = require('electron');
+const { connect, Client, setActivity, destroy } = require('discord-rpc');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -20,8 +21,8 @@ const createWindow = () => {
 	mainWindow.setMenuBarVisibility(false);
 
 	require('update-electron-app'), {
-		repo: 'h-projects/h',
-	}
+		repo: 'h-projects/h'
+	};
 
 	// and load the index.html of the app.
 	mainWindow.loadFile(`${__dirname}/index.html`);
@@ -48,3 +49,45 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+// create a new rpc client and track clicks
+const RPC = new Client({ transport:'websocket'});
+const clicks = await(mainWindow.webContents.executeJavaScript('UpdateHCount'));
+
+// create id and scopes
+const id = '712355400866988054';
+const scopes = ['rpc', 'rpc.api'];
+
+// connect when app is ready
+app.on('ready', () => {
+	connect();
+});
+
+// set activity
+setActivity({
+	details: `Clicking H`,
+	state: `${clicks} times(s)`,
+	largeImageKey: 'clicker',
+	largeImageText: 'H?',
+	smallImageKey: 'framecon',
+	smallImageText: 'Due to discord API limits, the h count updates every 15 seconds',
+	instance: false
+});
+
+// set activty when rpc is ready
+RPC.on('ready', () => {
+	setActivity();
+
+	// set interval
+	setInterval(() => {
+		setActivity();
+	  }, 15e3);
+});
+
+// destroy when all windows are closed (closes connection)
+app.on('window-all-closed', () => {
+	destroy();
+});
+
+// login
+RPC.login({ id, scopes });
